794c7efcf5cfa896864e3b5ddc3bc83d51c70496
Alumno <alumno@localhost.localdomain>
2014-03-18 09:56:31 -0600

    Imprimir pelotita

---
+++ my-gdx-game/src/com/me/mygdxgame/Pelotita.java
@@ -0,0 +1,26 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Pelotita extends Image{
+	
+	public Pelotita(int x,int y,Texture t) {
+		super(t);
+		this.addListener(new MiInput(this));
+		this.setX(x);
+		this.setY(y);
+	}
+ public void act
+ 
+	 (float deta){
+	 setX(getX()+1);
+	 if (getX()>500)
+	 {
+		setX(0);
+	}
+//	 if (getX()>100);
+//	 remove();
+ }
+}
+
